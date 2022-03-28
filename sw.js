eb689c8cd8c6b539d07829ed9205b31baf599413
Carlos Ortega <the_king_00carlos@hotmail.com>
2022-03-28 09:26:22 -0700

    fix sw routes and manifest desc

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("static").then((cache) => {
-      return cache.addAll(["./", "./assets/*.css", "./assets/*.png", "./assets/*.jpg", "./assets/*.otf"]);
-    }).catch(e => console.log(e))
+    caches
+      .open("static")
+      .then((cache) => {
+        return cache.addAll([
+          "./",
+          "./assets/index.1d5058d9.css",
+          "./assets/Bushido.f8b7c078.png",
+          "./assets/inventory.2b29f2de.png",
+          "./assets/nswitch.fd9ee3c4.png",
+          "./assets/ps5.270df83a.png",
+          "./assets/secure.139687f0.png",
+          "./assets/shop.cfcf8e14.png",
+          "./assets/steam.180e404d.png",
+          "./assets/utch.817ae100.png",
+          "./assets/xbox.738da36a.png",
+          "./assets/bg.5458cc8a.jpg",
+          "./assets/landscape.ba94889a.jpg",
+          "./assets/Road_Rage.45d908c5.otf",
+        ]);
+      })
+      .catch((e) => console.log(e))
   );
 });
 
