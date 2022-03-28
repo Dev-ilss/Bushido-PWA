14722eb07b6149eb1d629e76c60e5d0a181e55e8
Carlos Ortega <the_king_00carlos@hotmail.com>
2022-03-28 09:14:42 -0700

    sw log

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("static").then((cache) => {
       return cache.addAll(["./", "./assets/*.css", "./assets/*.png", "./assets/*.jpg", "./assets/*.otf"]);
-    })
+    }).catch(e => console.log(e))
   );
 });
 
